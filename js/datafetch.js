aff114712eff4e26ceb5afddb031b9aa8397d6e1
Samarpan Dutta <45854889+SamarpanDutta@users.noreply.github.com>
2021-03-15 05:42:43 -0400

    completed index and the model page

--- js/datafetch.js
+++ js/datafetch.js
@@ -1,11 +1,36 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-function reqListener () {
-    console.log(this.responseText);
+const axios = require('axios');
+const instance = axios.create({
+  baseURL: 'https://developer.nps.gov/api/v1',
+  method: 'get',
+  timeout: 1000,
+});
+
+instance.get('/activities?api_key=2tS2OFbpx613f4rFH61hm1KlduyObzOhyLVBXwBh')
+.then((response)=> {
+  objects = response.data['data'];
+  for (const obj of objects) {
+     for(const property in obj){
+       console.log(`${property}: ${obj[property]}`);
+     }
+   }
+})
+.catch(function (error){
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.log(error.response.data);
+    console.log(error.response.status);
+    console.log(error.response.headers);
+  } else if (error.request) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+    console.log(error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.log('Error', error.message);
   }
-  
-  var oReq = new XMLHttpRequest();
-  oReq.addEventListener("load", reqListener);
-  oReq.open("GET", "https://api.nal.usda.gov/fdc/v1/food/344647?api_key=GfjLqo42UhEqJl5036bQwPN9IRhrE1jJDtRX1y5S");
-  oReq.send();
+});
+
 
 
