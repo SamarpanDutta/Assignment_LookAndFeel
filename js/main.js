4e6d7628d9f9363900d9c53c3b47d224be0c8e10
Samarpan Dutta <45854889+SamarpanDutta@users.noreply.github.com>
2021-03-21 03:28:03 -0400

    Fixed Explore, Create and Search Page

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var ins = M.Sidenav.init(el,{});
     var el_c = document.querySelectorAll('.collapsible');
     var in_c = M.Collapsible.init(el_c,{});
+    // select element initialization
+    var el_d = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(el_d, {});
 });
 
 $(document).ready(function(){
